b77d7017d0168334f89d7a1cbb1d053d7f5dde8c
abhishekrajputsfdc10 <49307009+abhishekrajputsfdc10@users.noreply.github.com>
2023-02-13 14:04:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My Parents healthy dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('My Parents healthy dinner')
+streamlit.text('My Parents healthy dinner')
+streamlit.text('My Parents healthy dinner')
+
